f8f5b5e5991c9ec1b13ead29b7234f2f1b1eae3e
Ashwin Pahade <ashwinpahade97@gmail.com>
2025-12-09 11:23:39 +0530

    No changes has been made in this

--- project_1/firstApp/models.py
+++ project_1/firstApp/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+
+class person(models.Model):
+    id = models.BigAutoField(primary_key=True)
+    email = models.EmailField(default="")
+    name = models.CharField(max_length=255)
+    age = models.IntegerField(default=0)
